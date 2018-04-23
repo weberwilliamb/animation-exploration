1768f12488d8a56c1e1d4961f72d21ed2273ae62
Brian Bansenauer <brianb@cascadia.edu>
2018-04-22 23:02:56 -0700

    html-css-js

---
+++ animate.js
@@ -0,0 +1,9 @@
+/* ****************************************** */
+/* Progress Circle Animation using JS/jQuery  */ 
+/*      Add your JavaScript code here         */
+
+
+
+/* ****************************************** */
+/* Progress Circle Animation w/jQuery Library */ 
+/*      Add your JavaScript code here         */
